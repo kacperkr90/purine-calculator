947907133e448a5dc8c1562444aa5062e3fe7789
Kacper Krycki <kacper.krycki@bms.com.pl>
2017-06-12 16:24:31 +0200

    Renamed table FOOD to PRODUCT, db creation script included

--- app/src/main/java/pl/kacper90/purinecalculator/dao/Product.kt
+++ app/src/main/java/pl/kacper90/purinecalculator/dao/Product.kt
@@ -1,6 +1,6 @@
 package pl.kacper90.purinecalculator.dao
 
-class Food(val id: Long?, val name: String, val purineAcidValue: Long) {
+class Product(val id: Long?, val name: String, val purineAcidValue: Long) {
 
     constructor(name: String, purineAcidValue: Long) : this(null, name, purineAcidValue)
 
